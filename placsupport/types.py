6a37175c3573be388e418178eeedecca00f9943c
Ryan C. Thompson <rct@thompsonclan.org>
2011-04-11 01:25:09 -0700

    Add maybe_parse_int to types

--- placsupport/types.py
+++ placsupport/types.py
@@ -9,3 +9,10 @@ def positive_int(s):
     if x <= 0:
         raise ValueError("%s is not a positive integer" % (s,))
     return(x)
+
+def maybe_parse_int(s):
+    """Try to parse arg as int, else return unchanged"""
+    try:
+        return int(s)
+    except ValueError:
+        return s
